d351bdd2180e25a564a342f5077bba90f3fd96bb
Eric.Wu <eric140@ton-wa.com>
2022-02-22 15:55:37 +0800

    v1
    This reverts commit 69984011970052f0d203525d5b0dd76c9a1b839d.

--- Packer-基底系統/jdk11_install.sh
+++ Packer-基底系統/jdk11_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo wget -P /tmp https://cdn.azul.com/zulu/bin/zulu11.52.13-ca-jdk11.0.13-linux_x64.tar.gz
+#sudo wget -P /tmp https://cdn.azul.com/zulu/bin/zulu11.52.13-ca-jdk11.0.13-linux_x64.tar.gz
 sudo tar -zxvf /tmp/zulu11.52.13-ca-jdk11.0.13-linux_x64.tar.gz -C /tmp
 sudo mkdir /opt/JDK11 && sudo chmod 755 /opt/JDK11
 sudo mv /tmp/zulu11.52.13-ca-jdk11.0.13-linux_x64/* /opt/JDK11/
+sudo rm -rf /tmp/zulu11.52.13-ca-jdk11.0.13-linux_x64.tar.gz
 ##加入環境變數
 sudo sed -i '$a# Zulu JDK 11\nJAVA_HOME=/opt/JDK11\nCLASSPATH=$JAVA_HOME/lib/\nPATH=$PATH:$JAVA_HOME/bin/\nexport PATH JAVA_HOME CLASSPATH' /etc/profile
 sudo sed -i '$a# Zulu JDK 11\nJAVA_HOME=/opt/JDK11\nCLASSPATH=$JAVA_HOME/lib/\nPATH=$PATH:$JAVA_HOME/bin/\nexport PATH JAVA_HOME CLASSPATH' /etc/bashrc
--- Packer-基底系統/root-login.sh
+++ Packer-基底系統/root-login.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-##修改sshd_config
-#sudo sed -i 's/#PermitRootLogin no/PermitRootLogin yes/1' /etc/ssh/sshd_config
-#sudo sed -i '$aPermitRootLogin yes' /etc/ssh/sshd_config
-#sudo sed -i 's/UsePAM yes/UsePAM no/1' /etc/ssh/sshd_config
-##註解掉root ssh金鑰文件某段
-#sudo sed -ri 's/^/#/;s/sleep 10"\s+/&\n/' /root/.ssh/authorized_keys
-##重啟sshd服務
-#sudo systemctl restart sshd.service
 ##關閉ipv6功能
 sudo sed -i '$anet.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.conf
 sudo sed -i '$anet.ipv6.conf.default.disable_ipv6 = 1' /etc/sysctl.conf
@@ -21,20 +13,29 @@ sudo sysctl -p
 ##關閉 內建防火牆
 sudo systemctl stop firewalld.service && sudo systemctl disable firewalld.service
 ##更新
-#sudo yum update -y
+sudo yum update -y
 ##安裝套件
 sudo yum install -y nano && sudo yum install -y unzip && sudo yum install -y wget
 ##放入金鑰
 sudo mkdir ~/.aws && sudo mv /tmp/credentials ~/.aws/
+
+sleep 3s
+
+message "Install Node_exporter"
 ##Install Node_exporter
-sudo wget -P /tmp https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+#sudo wget -P /tmp https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 sudo tar -C /tmp/ -zxvf /tmp/node_exporter-1.3.1.linux-amd64.tar.gz
 sudo mkdir /etc/node_exporter
 sudo mv /tmp/node_exporter-1.3.1.linux-amd64/* /etc/node_exporter
-sudo chmod 757 /etc/systemd/system
-sudo cat <<\EOF > /etc/systemd/system/node_exporter.service
+sudo rm -rf /tmp/node_exporter-1.3.1.linux-amd64.tar.gz
+
+message "建立node_exporter開機啟動服務"
+sudo bash -c "cat > /etc/systemd/system/node_exporter.service" <<\EOF
 [Service]
 ExecStart=/etc/node_exporter/node_exporter
+Restart=always
+RestartSec=30
+StartLimitInterval=10
 
 [Install]
 WantedBy=multi-user.target
@@ -43,5 +44,5 @@ WantedBy=multi-user.target
 Description=node_exporter
 After=network.target
 EOF
-sudo chmod 755 /etc/systemd/system
+##Systemd開機啟動
 sudo systemctl daemon-reload && sudo systemctl enable node_exporter
